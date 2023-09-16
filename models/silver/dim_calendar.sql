c05a5551955e975f07017e2af59cbaaec6a5eb27
eoghan.mcnulty@gmail.com <eoghan.mcnulty@gmail.com>
2023-09-16 18:59:11 +0000

    updated dim_calendar to include 31/8/2023

--- models/silver/dim_calendar.sql
+++ models/silver/dim_calendar.sql
@@ -1,7 +1,7 @@
 -- create a calendar date table to support date filtering and aggregation across models
 {{ config(materialized="table") }}  -- build as table to optimise query performance
 
-with dates as ({{ dbt_date.get_date_dimension("2023-06-01", "2023-08-31") }})
+with dates as ({{ dbt_date.get_date_dimension("2023-06-01", "2023-09-01") }})
 
 select *
 from dates
